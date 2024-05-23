075e6aca99938b2cf8fef65990c520adde9df8cd
Gokul Y <gokulyc@gmail.com>
2024-05-23 11:45:59 +0530

    Added authentication

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.exceptions import NotFound
+from werkzeug.exceptions import NotFound, HTTPException
 from flask_jwt_extended import (
     JWTManager,
     current_user,
@@ -13,8 +13,7 @@ from flask_jwt_extended import (
 from models import User
 from forms import RegisterForm
 from config import create_flask_app
-import os
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, ObjectId
 
 app = create_flask_app()
 
@@ -36,7 +35,9 @@ def my_expired_token_callback(jwt_header, jwt_payload):
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
-    return User.query.filter(User.email == identity["email"]).one_or_none()
+    user = mongo.db.auth_users.find_one({"email": identity["email"]})
+    return user
+
 
 @app.route("/")
 def index():
@@ -49,9 +50,13 @@ def register():
     # print(form.data)
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data, method="scrypt")
-        result = mongo.db.auth_users.insert_one({"name": form.name.data,
-                                        "email": form.email.data,
-                                        "password": hashed_password})
+        result = mongo.db.auth_users.insert_one(
+            {
+                "name": form.name.data,
+                "email": form.email.data,
+                "password": hashed_password,
+            }
+        )
         user_id = result.inserted_id
         print(f"User added : {str(user_id)}")
         flash("User registered..., Please login.")
@@ -60,12 +65,43 @@ def register():
         print(form.errors)
     return render_template("register.html", form=form)
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    message = request.args.get("message")
+    if request.method == "POST":
+        user_result = mongo.db.auth_users.find_one({"email": request.form["email"]})
+        if user_result is None:
+            raise HTTPException("User/ Password invalid.")
+        user = User(**user_result)
+        if check_password_hash(user.password, request.form["password"]):
+            additional_claims = {**user.get_data(ignore_fields=["password"])}
+            print(f"{additional_claims=}")
+            access_token = create_access_token(
+                identity={"email": user.email},
+                additional_claims=additional_claims,
+            )
+            response = redirect("/dashboard")
+            set_access_cookies(response, access_token)
+            return response
+    return render_template("login.html", message=message)
+
+
 @app.route("/dashboard")
-# @jwt_required()
+@jwt_required()
 def dashboard():
-    # account: User = User.query.filter(User.email == current_user.email).one().to_dict()
-    # return render_template("dashboard.html", account=account)
-    return render_template("dashboard.html")
+    account: User = mongo.db.auth_users.find_one_or_404(
+        {"_id": ObjectId("664ed9f5df4b803e7381907d")}
+    )
+    return render_template("dashboard.html", account=User(**account))
+
+
+@app.route("/logout", methods=["GET"])
+def logout_with_cookies():
+    response = redirect("/")
+    unset_jwt_cookies(response)
+    return response
+
 
 if __name__ == "__main__":
     mongo.init_app(app)
--- src/models.py
+++ src/models.py
@@ -1,17 +1,14 @@
-from flask_pymongo import PyMongo, ObjectId
-
 
 class User:
-    def __init__(self, email, name, password, id = None) -> None:
-        if self.id:
-            self.id = ObjectId(id)
+    def __init__(self, email, name, password, _id = None) -> None:
+        self._id = _id
         self.email = email
         self.name = name
         self.password = password
 
-    def get_data(self, ignore_fields=["id", "password"]):
+    def get_data(self, ignore_fields=["_id", "password"]):
         di = {
-            "id": self.id,
+            "id": str(self._id),
             "name": self.name,
             "email": self.email,
             "password": self.password,
@@ -19,11 +16,3 @@ class User:
         for key in ignore_fields:
             di.pop(key)
         return di
-
-
-# class DB_Ops:
-#     def __init__(self, mongo_client: PyMongo) -> None:
-#         self.mongo= mongo_client
-
-#     def insert_record(self,collection,data, mongo: PyMongo):
-#         mongo.db.auth_users.insert_one({"email":})
